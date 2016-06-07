84aa8df4306096501c5fc06a9c5e0c8c66ceb215
Vamshi Krishna S <pavanvamsi3@users.noreply.github.com>
2016-06-07 21:19:51 +0530

    Update tempUrlFors3.py

--- tempUrlFors3.py
+++ tempUrlFors3.py
@@ -1,7 +1,12 @@
+# This script is used to give a signed url 
+# for a particular object in S3. We can mention
+# expiry time in seconds. After that the data is safe
+# only the download link is expired.
+# PS: Make sure you put the S3 Credentails in ~/.boto file.
 import boto
 
 con = boto.connect_s3()
 bucket = con.get_bucket('bucket-name')
 key = bucket.get_key('key-name')
-tempurl = key.generate_url(expires_in=86400) #Time in seconds
+tempurl = key.generate_url(expires_in=86400)
 print tempurl
