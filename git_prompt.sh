437a2dcae1b7a052b488fc1b69c7a7b0627037af
Vamshi Krishna S <pavanvamsi3@users.noreply.github.com>
2016-06-09 19:06:32 +0530

    Git prompt bash script.

---
+++ git_prompt.sh
@@ -0,0 +1,15 @@
+# Git branch in prompt.
+
+parse_git_branch() {
+
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+
+}
+
+export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+GIT_PS1_SHOWDIRTYSTATE=yes
+GIT_PS1_SHOWUNTRACKEDFILES=yes
+GIT_PS1_SHOWCOLORHINTS=yes
+source ~/.bash/git-prompt.sh
+PROMPT_COMMAND='__git_ps1 "${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]" "\\\$ "'
