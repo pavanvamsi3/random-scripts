f183e4532231a552d3502c3aa6fa7c4eecfb2902
Vamsi <pavanvamsi3@gmail.com>
2018-06-26 17:01:34 +0530

    Create vs_code.py

---
+++ vs_code.py
@@ -0,0 +1,10 @@
+{
+    "workbench.colorTheme": "Visual Studio Dark",
+    "editor.rulers": [
+        79
+    ],
+    "editor.renderWhitespace": "boundary",
+    "files.insertFinalNewline": true,
+    "files.trimFinalNewlines": true,
+    "files.trimTrailingWhitespace": true,
+}
