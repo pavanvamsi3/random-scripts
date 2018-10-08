228d14cc2c8bd7f5b62b4f2a19bd87c43e62395a
Vamsi <pavanvamsi3@gmail.com>
2018-10-08 16:27:50 +0530

    Update generate_certificate.sh

--- generate_certificate.sh
+++ generate_certificate.sh
@@ -1,11 +1,11 @@
-// This will generate the server key, asks to enter passphrase
+# This will generate the server key, asks to enter passphrase
 openssl genrsa -aes128 -out server.key 2048
 
-// This command will remove the passphrase
+# This command will remove the passphrase
 openssl rsa -in server.key -out server.key
 
-// Creating a CSR 
+# Creating a CSR 
 openssl req -new -days 365 -key server.key -out server.csr
 
-// Generating a certificate
+# Generating a certificate
 openssl x509 -in server.csr -out server.crt -req -signkey server.key -days 365
