af48da2acaee052ca500ef09d47a6202d082e0df
Pavan Vamsi <pavanvamsi3@gmail.com>
2020-05-12 05:06:27 +0530

    Create scrum_random.py

---
+++ scrum_random.py
@@ -0,0 +1,25 @@
+import random
+
+members = ["AJ",
+"Deepak",
+"Rajendra",
+"Zainu",
+"Ambika",
+"Soubhagya",
+"Raghav",
+"Koushik",
+"Tauhid",
+"Jyotsna",
+"Akshansh",
+"Ankit",
+"Rachit",
+"Ganesh",
+"Shivam",
+"Aman",
+"Rohit",
+"Vamsi"]
+
+random.shuffle(members)
+
+for teammate in members:
+    print (teammate)
